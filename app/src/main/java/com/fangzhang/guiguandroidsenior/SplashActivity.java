99182a38a9560adc6fecb1aa6290cc748655dcfd
MoeRookie <18401430395@163.com>
2017-11-27 16:54:14 +0800

    一、开始搭建软件框架 .

--- app/src/main/java/com/fangzhang/guiguandroidsenior/SplashActivity.java
+++ app/src/main/java/com/fangzhang/guiguandroidsenior/SplashActivity.java
@@ -3,6 +3,9 @@ package com.fangzhang.guiguandroidsenior;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 1. 开始搭建软件框架
+ */
 public class SplashActivity extends AppCompatActivity {
 
     @Override

b952ca97b1fae9b20a5037617abbb63c1ee48036
MoeRookie <18401430395@163.com>
2017-11-27 17:44:49 +0800

    Android 软件框架搭建 - Splash延迟跳转主界面 .

--- app/src/main/java/com/fangzhang/guiguandroidsenior/SplashActivity.java
+++ app/src/main/java/com/fangzhang/guiguandroidsenior/SplashActivity.java
@@ -1,5 +1,7 @@
 package com.fangzhang.guiguandroidsenior;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,5 +14,23 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            /**
+             * 此处代码在主线程中运行
+             */
+            @Override
+            public void run() {
+                startMainActivity();
+            }
+        }, 2000);
+    }
+
+    /**
+     * 启动主页面
+     */
+    private void startMainActivity() {
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }

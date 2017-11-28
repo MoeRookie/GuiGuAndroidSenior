85b3a9d6714617f599119136a4ea264bb010df61
MoeRookie <18401430395@163.com>
2017-11-28 09:30:46 +0800

    Android 软件框架搭建 - 完成主界面布局，设置默认显示第一个底部菜单项被选中 .

--- app/src/main/java/com/fangzhang/guiguandroidsenior/MainActivity.java
+++ app/src/main/java/com/fangzhang/guiguandroidsenior/MainActivity.java
@@ -2,14 +2,19 @@ package com.fangzhang.guiguandroidsenior;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
+import android.widget.RadioGroup;
 
 /**
  * Created by Administrator on 2017/11/27.
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
+    private RadioGroup mRgBottomTag;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mRgBottomTag = findViewById(R.id.rg_bottom_tag);
+        mRgBottomTag.check(R.id.rb_common_frame);
     }
 }

f5465cc85735cfaa1f5acd0e5d7bd43e9ab56a4d
MoeRookie <18401430395@163.com>
2017-11-29 09:27:18 +0800

    Android 软件框架搭建 - 底部菜单被点击时切换到对应的Fragment .

--- app/src/main/java/com/fangzhang/guiguandroidsenior/MainActivity.java
+++ app/src/main/java/com/fangzhang/guiguandroidsenior/MainActivity.java
@@ -2,7 +2,10 @@ package com.fangzhang.guiguandroidsenior;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.widget.RadioGroup;
 
 import com.fangzhang.guiguandroidsenior.base.BaseFragment;
@@ -20,12 +23,72 @@ import java.util.List;
 public class MainActivity extends FragmentActivity {
     private RadioGroup mRgBottomTag;
     private List<BaseFragment> mFragmentList;
+    private int position;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
         initFragment();
+        // 设置RadioGroup的监听
+        setListener();
+    }
+
+    /**
+     * 设置RadioGroup的监听
+     */
+    private void setListener() {
+        mRgBottomTag.setOnCheckedChangeListener(checkedChangeListener);
+    }
+    RadioGroup.OnCheckedChangeListener checkedChangeListener = new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            switch (checkedId) {
+                case R.id.rb_common_frame:
+                    position = 0;
+                    break;
+                case R.id.rb_party_third:
+                    position = 1;
+                    break;
+                case R.id.rb_custom:
+                    position = 2;
+                    break;
+                case R.id.rb_other:
+                    position = 3;
+                    break;
+                default:
+                    position = 0;
+                    break;
+            }
+            BaseFragment fragment = getFragment();
+            switchFragment(fragment);
+        }
+    };
+
+    /**
+     * 切换显示对应的Fragment
+     */
+    private void switchFragment(BaseFragment fragment) {
+        // 得到FragmentManager
+        FragmentManager manager = getSupportFragmentManager();
+        // 开启事务
+        FragmentTransaction transaction = manager.beginTransaction();
+        // 替换显示
+        transaction.replace(R.id.fl_content,fragment);
+        // 提交事务
+        transaction.commit();
+    }
+
+    /**
+     * 得到Fragment
+     * @return
+     */
+    private BaseFragment getFragment() {
+        if (mFragmentList != null) {
+            BaseFragment fragment = mFragmentList.get(position);
+            return fragment;
+        }
+        return null;
     }
 
     /**
@@ -37,6 +100,9 @@ public class MainActivity extends FragmentActivity {
         mFragmentList.add(new ThirdPartyFragment());
         mFragmentList.add(new CustomFragment());
         mFragmentList.add(new OtherFragment());
+        // 设置默认显示第一个Fragment
+        mRgBottomTag.check(R.id.rb_common_frame);
+        switchFragment(getFragment());
     }
 
     /**

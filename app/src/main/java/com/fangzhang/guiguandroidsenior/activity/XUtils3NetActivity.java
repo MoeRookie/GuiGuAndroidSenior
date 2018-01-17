1d0adc21f84d0fb708cf031218f48f5dce3b586c
MoeRookie <18401430395@163.com>
2018-01-17 17:13:04 +0800

    xUtils3 - 下载大文件 .

--- app/src/main/java/com/fangzhang/guiguandroidsenior/activity/XUtils3NetActivity.java
+++ app/src/main/java/com/fangzhang/guiguandroidsenior/activity/XUtils3NetActivity.java
@@ -2,6 +2,7 @@ package com.fangzhang.guiguandroidsenior.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -12,11 +13,14 @@ import android.widget.Toast;
 import com.fangzhang.guiguandroidsenior.R;
 
 import org.xutils.common.Callback;
+import org.xutils.common.task.PriorityExecutor;
 import org.xutils.http.RequestParams;
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.Event;
 import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
+
+import java.io.File;
 
 /**
  * Created by Administrator on 2018/1/17.
@@ -49,12 +53,64 @@ public class XUtils3NetActivity extends Activity{
                 getAndPostNet();
                 break;
             case R.id.btn_download_file:
-                Toast.makeText(XUtils3NetActivity.this, "文件下载", Toast.LENGTH_SHORT).show();
+                downloadFile();
                 break;
             case R.id.btn_upload_file:
                 Toast.makeText(XUtils3NetActivity.this, "文件上传", Toast.LENGTH_SHORT).show();
                 break;
         }
+    }
+
+    private void downloadFile() {
+        RequestParams entity = new RequestParams("http://vfx.mtime.cn/Video/2016/09/15/mp4/160915092608935956_480.mp4");
+        entity.setSaveFilePath(Environment.getExternalStorageDirectory()+"/FangZhang/480.mp4");
+        // 设置可以取消下载
+        entity.setCancelFast(true);
+        // 设置是否可以自动根据头信息命名
+        entity.setAutoRename(false);
+        // 设置断点续传
+        entity.setAutoResume(true);
+        // 自定义线程池,有效值范围[1,3],设置为3时,可能阻塞图片加载
+        entity.setExecutor(new PriorityExecutor(3,true));
+        x.http().get(entity, new Callback.ProgressCallback<File>() {
+            @Override
+            public void onWaiting() {
+                Log.e(TAG, "onWaiting==");
+            }
+
+            @Override
+            public void onStarted() {
+                Log.e(TAG, "onStarted==");
+            }
+
+            @Override
+            public void onLoading(long total, long current, boolean isDownloading) {
+                mProgressBar.setMax((int) total);
+                mProgressBar.setProgress((int) current);
+                Log.e(TAG, "onLoading="+current+"/"+total+",isDownloading="+isDownloading);
+            }
+
+            @Override
+            public void onSuccess(File result) {
+                Log.e(TAG, "onSuccess=="+result.toString());
+                Toast.makeText(XUtils3NetActivity.this, result.toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+                Log.e(TAG, "onError=="+ex.getMessage());
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+                Log.e(TAG, "onCancelled=="+cex.getMessage());
+            }
+
+            @Override
+            public void onFinished() {
+                Log.e(TAG, "onFinished==");
+            }
+        });
     }
 
     private void getAndPostNet() {

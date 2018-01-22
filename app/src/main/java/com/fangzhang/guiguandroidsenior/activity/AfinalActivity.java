c43c1fc28f9854d144115ed379cd5d519b9449d0
MoeRookie <MoeRookie@163.com>
2018-01-22 13:38:38 +0800

    Afinal - 加载文本

--- app/src/main/java/com/fangzhang/guiguandroidsenior/activity/AfinalActivity.java
+++ app/src/main/java/com/fangzhang/guiguandroidsenior/activity/AfinalActivity.java
@@ -10,7 +10,9 @@ import com.fangzhang.guiguandroidsenior.R;
 
 import net.tsz.afinal.FinalActivity;
 import net.tsz.afinal.FinalBitmap;
+import net.tsz.afinal.FinalHttp;
 import net.tsz.afinal.annotation.view.ViewInject;
+import net.tsz.afinal.http.AjaxCallBack;
 
 /**
  * Created by Administrator on 2018/1/22.
@@ -48,7 +50,28 @@ public class AfinalActivity extends FinalActivity {
         finalBitmap.display(mivFinal,"http://img5.mtime.cn/mg/2016/10/11/160347.30270341.jpg");
     }
     public void onGetTextClicked(View view){
-        Toast.makeText(AfinalActivity.this, "获取文本", Toast.LENGTH_SHORT).show();
+        FinalHttp finalHttp = new FinalHttp();
+        String url = "http://api.m.mtime.cn/PageSubArea/TrailerList.api";
+        finalHttp.get(url, new AjaxCallBack<Object>() {
+            @Override
+            public void onStart() {
+                mtvResult.setText("开始加载");
+                super.onStart();
+            }
+
+            @Override
+            public void onSuccess(Object o) {
+                // 加载成功后显示结果
+                mtvResult.setText(o.toString());
+                super.onSuccess(o);
+            }
+
+            @Override
+            public void onFailure(Throwable t, int errorNo, String strMsg) {
+                mtvResult.setText("加载失败");
+                super.onFailure(t, errorNo, strMsg);
+            }
+        });
     }
     public void onLoadFileClicked(View view){
         Toast.makeText(AfinalActivity.this, "下载文件", Toast.LENGTH_SHORT).show();
